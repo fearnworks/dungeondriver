90ea8d20f47bdee5bb2abb3193fff7608f6263f0
jphillips <josh.phillips@fearnworks.com>
2023-07-30 11:08:08 -0500

    Add dungeondriver module

--- ai_driver/data/README.md
+++ ai_driver/data/README.md
@@ -1,3 +1,3 @@
 # DISCLAIMER
 
-Only open source or publicly released materials are allowed in this repository. Any privately owned or licensed material is forbidden. 
+Only open source or publicly released materials are allowed in this repository. Any privately owned or licensed material is forbidden.  
---
+++ dungeon_driver/dungeon_driver/tests/dd_test.py
@@ -0,0 +1,26 @@
+from dungeon_driver.spell_surge_generator import SpellSurgeGenerator, ReplacementConfig
+
+def test_generate_subject_replacement():
+    # Given
+    prompt = "The caster casts a spell on the target."
+    generator = SpellSurgeGenerator()
+
+    replacement_config = ReplacementConfig(target="Orc Warrior", caster="Human Mage")
+    # When
+    output = generator.generate_subject_replacement(prompt, replacement_config)
+
+    # Then
+    assert output == "The Human Mage casts a spell on the Orc Warrior."
+
+def test_roll_dice_in_string():
+    # Given
+    s = "The caster rolls 2d6 for damage."
+    generator = SpellSurgeGenerator()
+
+    # When
+    output = generator.roll_dice_in_string(s)
+
+    # Then
+    assert "2d6" not in output
+    assert isinstance(output, str)
+
