868fb71a0170870b5daaa4d2aee68e577f6845c5
jphillips <josh.phillips@fearnworks.com>
2023-08-03 15:10:39 -0500

    Decouple server_config from langsmith_config

--- ai_driver/ai_driver/langsmith_config.py
+++ ai_driver/ai_driver/langsmith_config.py
@@ -2,18 +2,15 @@ import os
 from loguru import logger
 from langsmith import Client as LangsmithClient
 
-LANGCHAIN_TRACING_V2 = os.environ.get("LANGCHAIN_TRACING_V2")
 LANGCHAIN_PROJECT = os.environ.get("LANGCHAIN_PROJECT")
 LANGCHAIN_ENDPOINT = os.environ.get("LANGCHAIN_ENDPOINT")
 LANGCHAIN_API_KEY = os.environ.get("LANGCHAIN_API_KEY")
 
-from ai_driver.config import server_config
-
 
 def get_client():
     """Default client for langchain"""
-    logger.info(server_config.LANGSMITH_LOGGING)
-    if server_config.LANGSMITH_LOGGING:
+    LANGCHAIN_TRACING_V2 = os.environ.get("LANGCHAIN_TRACING_V2")
+    if LANGCHAIN_TRACING_V2:
         return LangsmithClient(api_url=LANGCHAIN_ENDPOINT, api_key=LANGCHAIN_API_KEY)
     else:
         logger.info("Langsmith disabled")
