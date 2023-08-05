84d0b53d76d8db6005477e3678a21a3a3f8f66a2
jphillips <josh.phillips@fearnworks.com>
2023-08-05 06:55:37 -0500

    Fix dir concat

--- scripts/init_local_vectorstore.py
+++ scripts/init_local_vectorstore.py
@@ -14,7 +14,7 @@ def from_yaml(filepath):
             logger.error(f"Error loading config file: {exc}")
             raise exc
         kwargs = {
-            "data_path": data.get("." + "DATA_PATH"),
+            "data_path": "." + data.get("DATA_PATH"),
             "chunk_size": data.get("CHUNK_SIZE"),
             "chunk_overlap": data.get("CHUNK_OVERLAP"),
             "chat_embed_model": data.get("CHAT_EMBED_MODEL"),
@@ -25,9 +25,11 @@ def from_yaml(filepath):
 
 def run_db_build():
     # Load configuration from YAML file
+    logger.info("Loading configuration")
     config = from_yaml("./ai_driver/config/config.yaml")
-
+    logger.info(config)
     # Build vector database
+    logger.info("Building vector database")
     loader = DirectoryLoader(config["data_path"], glob="*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
@@ -42,6 +44,7 @@ def run_db_build():
 
     vectorstore = FAISS.from_documents(texts, embeddings)
     vectorstore.save_local(config["db_path"])
+    logger.info("Vectorestore Init Done!")
 
 
 if __name__ == "__main__":
