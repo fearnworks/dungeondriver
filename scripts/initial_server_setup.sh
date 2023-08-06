3262e7ccd969c8bbd184d88dadb4de41d451d328
jphillips <josh.phillips@fearnworks.com>
2023-08-06 07:29:32 -0500

    Force use of 3.10 executable

--- scripts/initial_server_setup.sh
+++ scripts/initial_server_setup.sh
@@ -28,14 +28,14 @@ done
 
 sudo docker network create dd-network # handle for folks who don't have docker groups set up
 
-python3 -m venv .server_venv && source .server_venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install -r ./ai_driver/requirements.txt
+python3.10 -m venv .server_venv && source .server_venv/bin/activate
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r ./ai_driver/requirements.txt
 
 if [ "$DOWNLOAD_MODEL" = true ]; then
-  python3 ./ai_driver/ai_driver/scripts/download_model.py
+  python3.10 ./ai_driver/ai_driver/scripts/download_model.py
 fi
 
 if [ "$INIT_VECTORSTORE" = true ]; then
-  python3 ./scripts/init_local_vectorstore.py
+  python3.10 ./scripts/init_local_vectorstore.py
 fi
