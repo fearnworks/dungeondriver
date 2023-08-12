037c87023e9cfc7a78bb4df5051dc501a46b05e1
jphillips <josh.phillips@fearnworks.com>
2023-08-12 04:57:58 -0500

    Add ability to pull history

--- dungeon_driver/dungeon_driver/webui/chat_tab.py
+++ dungeon_driver/dungeon_driver/webui/chat_tab.py
@@ -31,12 +31,32 @@ async def chat(chat_history, message: str, session_id: str):
     return history
 
 
+async def get_history(session_id: str):
+    async with httpx.AsyncClient(timeout=timeout) as client:
+        # Call add_auth_headers to include the authentication token in the headers
+        client = auth_service.add_auth_headers(client)
+
+        logger.info(f"Making request to history with session {session_id}")
+        request = {"session_id": session_id}
+        response = await client.post(
+            f"http://ai_driver:28001/api/v1/chat/cloudllm/history",
+            json=request,
+        )
+        logger.debug(response.text)
+        history_pairs = response.json()["history"]
+        history = []
+        for pair in history_pairs:
+            history.append((pair["human"], pair["ai"]))
+
+        return history
+
+
 def create_chat_interface() -> gr.Blocks:
     session_id = secrets.randbits(32)
     with gr.Blocks() as chat_tab:
         gr.Markdown("# Chatbot")
         with gr.Row():
-            chat_history = [("Hello", "Hi!")]
+            chat_history = []
             chat_display = gr.Chatbot(
                 chat_history,
                 elem_id="chatbot",
@@ -50,6 +70,10 @@ def create_chat_interface() -> gr.Blocks:
                 chat,
                 inputs=[chat_display, chat_input, session_id_label],
                 outputs=[chat_display],
+            )
+            refresh_button = gr.Button("Refresh History")
+            refresh_button.click(
+                get_history, inputs=[session_id_label], outputs=[chat_display]
             )
 
     return chat_tab
